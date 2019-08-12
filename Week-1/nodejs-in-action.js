8f2bce505c1adeec046db7c8da9854b63aba878f
Karie Funk <kfunk@my365.bellevue.edu>
2019-08-12 11:00:31 -0600

    update

---
+++ Week-1/nodejs-in-action.js
@@ -0,0 +1,37 @@
+
+
+//HEADER
+var header = require('./Funk-header.js');
+
+console.log(header.display("Karie", "Funk", "Exercise 1.5"));
+
+console.log("");
+
+//START PROGRAM
+
+var http = require("http");
+
+function processRequest(req, res) {
+
+//Remade with my own body message
+var body = "Hello World, my name is Karie Funk";
+
+    var contentLength = body.length;
+
+    res.writeHead(200, {
+
+        'Content-Length': contentLength,
+
+        'Content-Type': 'text/plain'
+
+    });
+
+    res.end(body);
+
+}
+
+var s = http.createServer(processRequest);
+
+s.listen(8080);
+
+//END PROGRAM
