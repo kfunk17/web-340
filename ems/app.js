c40247d2f118eb5653a0b9df53060d402c2ddba3
Karie Funk <kfunk@my365.bellevue.edu>
2019-09-16 20:29:02 -0600

    update

--- ems/app.js
+++ ems/app.js
@@ -1,31 +1,31 @@
-var express = require("express");
+/*
+============================================
+; Title: EMS APP.JS
+; Author: Karie Funk
+; Date: 8 September 2019
+; Description: This will be an app
+; showing employee records.
+;===========================================
+*/
 
-var http = require("http");
-
-var path = require("path");
-
-var logger = require("morgan");
-
+//requirements 
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
-
 app.set("view engine", "ejs");
+app.use(logger('short'));
 
-app.use(logger("short"));
-
-app.get("/", function (request, response) {
-
-    response.render("index", {
-
-        title: "Home page"
-
-    });
-
+app.get("/", function(req, res) {
+  res.render("index", {
+    title: "Home Page",
+  });
 });
 
+//created server and listen on a port at 8080
 http.createServer(app).listen(8080, function() {
-
-    console.log("Application started on port 8080!");
-
+  console.log("Application started on port 8080!");
 });
